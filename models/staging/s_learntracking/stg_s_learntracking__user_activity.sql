e14c157cee365fad23ab9a7e25db9afd59676863
Doug Jeffery <doug@dataops.me.uk>
2024-02-06 14:47:55 +0000

    v1 for review
    hopefully covered the task :)

--- models/staging/s_learntracking/stg_s_learntracking__user_activity.sql
+++ models/staging/s_learntracking/stg_s_learntracking__user_activity.sql
@@ -1,3 +1,10 @@
+{{
+    config(
+        unique_key='id',
+        on_schema_change='fail' -- DJ 2024/02/06 we could choose a different option here obvs
+    )
+}}
+
 select
 	_id as id,
 	_fivetran_synced as _synced_at,
@@ -7,3 +14,13 @@ select
 	type,
 	params,
 from {{ source('mojo_s_learntracking', 'user_activity') }}
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  -- uses > to include records which were synced since the last run of this model
+  -- applies a negative tolerance to handle sync variance
+  -- future idea - use knowledge of the schedule to avoid full scan to get max(synced)??
+  where _synced_at > (select max( {{ dateadd("minute", -{{ var("timestamp_tolerance_mins") }}, _synched_at) }} ) from {{ this }}
+
+{% endif %}
---
+++ tests/marts/activity/user_activity_sense_check.sql
@@ -0,0 +1,23 @@
+select *,
+from {{ ref('course_activity') }}
+where
+	type in ('course_completed')
+	and json_type(params.course_key) != 'string'
+
+with user_course_summary as (
+    select
+      user_id,
+      json_type(params.course_key) as course_key,
+      sum(case when type = 'course_started' then 1 else 0 end) as course_starts,
+      max(case when type = 'course_started' then created_at else null end) as max_course_start,
+      sum(case when type = 'course_completed' then 1 else 0 end) as course_ends,
+      max(case when type = 'course_completed' then created_at else null end) as max_course_end,
+    from {{ ref('course_activity') }}
+    group by 1, 2
+)
+select *
+from user_course_summary
+where
+    course_starts > 1
+    or course_ends > 1
+    or (max_course_end is not null and max_course_end < max_course_start)
